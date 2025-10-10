5498cb5166dbc4ecd3abb962a79965796e1e409d
Noah <kidclark27@gmail.com>
2025-10-10 16:51:37 -0400

    Update touchpadDriverTest.java

--- touchpadDriverTest.java
+++ touchpadDriverTest.java
@@ -1,5 +1,7 @@
 public class TouchpadDriverTest1 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main() {
+    if(gamepad.1_touchpad_finger_1){
+        telemetry.adddata("You put 1 finger on the touchpad at this location /n x:"+gamepad.1_touchpad_finger_1_x+"/n y:"+gamepad.1_touchpad_finger_1_y+"/n Thank you for testing!");
+        telemetry.update();
     }
 }
