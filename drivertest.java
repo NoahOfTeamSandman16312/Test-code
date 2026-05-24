91d9e238539f9a2eccee8840039fc217dee2d603
Noah <kidclark27@gmail.com>
2026-05-24 14:48:08 -0400

    Fix class declaration and add telemetry data

--- drivertest.java
+++ drivertest.java
@@ -5,4 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-public
+public class driverTest{
+  public static main void{
+
+  telemetry.adddata(
